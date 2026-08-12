a150ce61bd509f95e83dda036630170b30f6c31f
Gabriel Romanovski Vitorasse <vitorassegabriel@gmail.com>
2026-08-12 10:43:00 -0300

    Add files via upload

---
+++ aula20260812/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+    /*void imprime (int *m, int n){
+        int i;
+        for(i=0; i<n;i++){
+            printf("%d=\t%d \n", i+1, m[i]);
+        }
+    }*/
+
+    void imprime (int *m, int n){
+        if()
+    }
+
+    /*int calcular (int *m, int n){
+        if(n==0) return 1;
+        else return m[n] + calcular(m, n-1);
+    }*/
+
+    int main(){
+        int vet[5] = {1,2,3,4,5}, n;
+        printf("==============================================\n");
+        printf("\tConsultor de numeros perfeitos.\n");
+        printf("==============================================\n");
+        imprime(vet,5);
+        //n = calcular(vet,5);
+        //printf("total: %d", n);
+
+        return 0;
+    }
